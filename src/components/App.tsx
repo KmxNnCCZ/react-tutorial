b4e556add7ff178333d69e614b70fb442232afbf
ああああああ <mmmm48698062@icloud.com>
2023-11-25 12:45:50 +0900

    最初のコミット

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+function Profiele() {
+  return (
+    <img 
+      src="https://i.imgur.com/MK3eW3As.jpg" 
+      alt="Katherine Johnson" 
+    />
+  );
+}
+
+function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profiele />
+      <Profiele />
+      <Profiele />
+    </section>
+  )
+}
+
+export default Gallery
